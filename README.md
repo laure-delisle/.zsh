ceb84df091d869d46dcffc4b714e6517ccaac995
laure-delisle <laure.delisle@gmail.com>
2018-04-06 15:38:40 +0100

    deploy configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# zsh_config
+
+### for colors in man pages:
+
+`sudo apt-get install most`
+
+### run:
+
+`./deploy.sh`
---
+++ deploy.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install vim tmux most virtualenv zsh python-pip git
+
+# in case the repo was not cloned at home
+mv ./.zsh ~/.zsh
+
+# deploy .zshrc
+mv ./zshrc ~/.zshrc
+
